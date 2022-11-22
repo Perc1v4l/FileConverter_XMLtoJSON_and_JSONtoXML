2129f36f389d5a659a1f62fccd24ccac5e4da793
Perc1v4l <sansan17volodin@mail.ru>
2022-11-22 19:21:15 +0300

    Updated Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import Convertor.Converter;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception
+    {
+        Converter f = new Converter();
+
+        if(args.length!=0)
+        {
+            if(args[0].contains(".json"))
+                f.convertToXML(args[0],args[1]);
+            else if(args[0].contains(".xml"))
+                f.convertToJson(args[0], args[1]);
+            else throw new Exception("Wrong input!");
+        }
+        else {
+            f.convertToXML("E:\\3курс\\pr2_maven\\src\\test\\BBBBBB.json", "E:\\3курс\\pr2_maven\\src\\test\\AAAAAA.xml");
+            f.convertToJson("E:\\3курс\\pr2_maven\\src\\test\\Nikita.xml", "E:\\3курс\\pr2_maven\\src\\test\\Matthew.json");
+        }
     }
 }
